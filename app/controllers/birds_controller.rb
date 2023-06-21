f35382a8d6fd320462126f93fdd4fa2808bab687
kk <+>
2023-06-21 14:35:56 +0300

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,6 +5,10 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 
   # GET /birds/:id
   def show
